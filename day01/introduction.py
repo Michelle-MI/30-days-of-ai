4f1e6b6019e232f9a7d3bdbeafc67fc339cd1cb4
Michelle <muenimichelle21@gmail.com>
2026-07-08 21:11:19 +0300

    Complete Day01

---
+++ day01/introduction.py
@@ -0,0 +1,20 @@
+# Day 1 - AI Engineering Journey
+
+name = "Michelle"
+career_goal = "AI Engineer"
+days = 30
+university = "University of Nairobi"
+course = "Computer Science"
+graduation_year = 2027
+programming_language = "Python"
+
+print("Hello,", name + "!")
+print("Welcome to your AI Engineering Journey.")
+print("My goal is to become an", career_goal)
+print("I am committing to", days,"days of consistent learning.")
+print("I am a fourth-year", course, "student at the", university, ".")
+print("I will be graduating in", graduation_year, "and my dream job is to be an", career_goal, ".")
+print("My favorite programming language is", programming_language, ".")
+
+
+
