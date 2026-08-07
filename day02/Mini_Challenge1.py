358f26efd2fe8f3330ee297e53a8f1244939d957
Michelle <muenimichelle21@gmail.com>
2026-08-07 16:43:37 +0300

    Day02 challenge question

--- day02/Mini_Challenge1.py
+++ day02/Mini_Challenge1.py
@@ -1,5 +1,11 @@
 # Mini Challenge 1
+# Modify the program so it asks for:
 
+# Name
+# University
+# Course
+
+# Then print a friendly introduction.
 name = input("What is your name? ")
 university = input("What university do you attend? ")
 course = input("What course are you taking? ")
